ee0484788678e03bbe8983c6558d383b8d4f05f1
anteisuba <xiuruisu@gmail.com>
2023-10-27 19:36:24 +0800

    2023.10.27 条件：和

---
+++ Reservation.java
@@ -0,0 +1,34 @@
+public class Reservation{
+	int guestCount;
+	int restaurantCapacity;
+	boolean isRestaurantOpen;
+	boolean isConfirmed;
+	
+	public Reservation(int count,int capacity,boolean open) {
+		guestCount = count;
+		restaurantCapacity = capacity;
+		isRestaurantOpen = open;
+	}
+	public void confirmReservation() {
+		if(restaurantCapacity >= guestCount && isRestaurantOpen) {
+			System.out.println("Reservation confirmed");
+			isConfirmed = true;
+		}else {
+			System.out.println("Reservation denid");
+			isConfirmed = false;
+		}
+		
+	}
+	public void informUser() {
+		System.out.println("Please enjoy your meal!");
+	}
+	
+	public static void main(String[] args) {
+		Reservation partOfThree = new Reservation(3,12,true);
+		Reservation partOfFour = new Reservation(4,3,true);
+		partOfThree.confirmReservation();
+		partOfThree.informUser();
+		partOfFour.confirmReservation();
+		partOfFour.informUser();
+	}
+}
