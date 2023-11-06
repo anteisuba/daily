d7948fea957d99d9467735da8214834702bbb6d0
anteisuba <xiuruisu@gmail.com>
2023-11-06 19:42:24 +0800

    添加注释

--- GetElement.java
+++ GetElement.java
@@ -26,12 +26,12 @@ public class GetElement {
     
     System.out.println("The top topic is "+ sampleFeed.getTopTopic());
     
-    sampleFeed.viewTopic(1);
-    sampleFeed.viewTopic(1);
-    sampleFeed.viewTopic(3);
-    sampleFeed.viewTopic(2);
-    sampleFeed.viewTopic(2);
-    sampleFeed.viewTopic(1);
+    sampleFeed.viewTopic(1);   //views[1] = 1 views=0,1,0,0
+    sampleFeed.viewTopic(1);   //views[1] = 2 views=0,2,0,0
+    sampleFeed.viewTopic(3);   //views[3] = 1 views=0,2,0,1
+    sampleFeed.viewTopic(2);   //views[2] = 1 views=0,2,1,1
+    sampleFeed.viewTopic(2);   //views[2] = 2 views=0,2,2,1
+    sampleFeed.viewTopic(1);   //views[1] = 3 views=0,3,2,1
     
     System.out.println("The " + sampleFeed.topics[1] + " topic has been viewed " + sampleFeed.views[1] + " times!"); 
   }
